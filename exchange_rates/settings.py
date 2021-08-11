caa72e81bee70f2f4303a59d25fa8e988c4d9745
Vladislav <sidorenkov.slava@gmail.com>
2021-08-11 18:12:53 +0300

    Add docker, docker-compose

--- exchange_rates/settings.py
+++ exchange_rates/settings.py
@@ -1,10 +1,18 @@
 from pathlib import Path
+import os
+
+import environ
+
+env = environ.Env()
+environ.Env.read_env(env_file='.env')
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = 'django-insecure-uewdoud+(9uvco#8siday#waauxw217@d1z-12*c8@49*698il'
-DEBUG = True
 
-ALLOWED_HOSTS = []
+SECRET_KEY = env('SECRET_KEY')
+DEBUG = env.bool('DEBUG', False)
+
+ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS += env.list('ALLOWED_HOSTS', default=[])
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -49,11 +57,21 @@ TEMPLATES = [
 WSGI_APPLICATION = 'exchange_rates.wsgi.application'
 
 DATABASES = {
-    'default': {
+    'develop': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'production': {
+        'ENGINE': env('DB_ENGINE'),
+        'NAME': env('DB_NAME'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
     }
 }
+
+DATABASES['default'] = DATABASES['develop' if DEBUG else 'production']
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -93,5 +111,9 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
