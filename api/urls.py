ccb91bbfe11dafafc6306dc224db3acd3e308f58
Vladislav <sidorenkov.slava@gmail.com>
2021-08-11 19:23:08 +0300

    Add swagger

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,10 @@
 from django.urls import include, path
 from django.views.generic import RedirectView
 from rest_framework.authtoken.views import obtain_auth_token
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
 
 from .router import RateRouter
 from .views import RateViewSet
@@ -12,4 +16,28 @@ urlpatterns = [
     path('', RedirectView.as_view(pattern_name='currencies-list')),
     path('auth/', obtain_auth_token, name='auth'),
     path('', include(router.urls)),
+]
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='API проекта Exchange rates',
+        default_version='v1',
+        description='Проект выполнен в рамках тестового задания',
+        contact=openapi.Contact(
+            name='Владислав',
+            email='sidorenkov.v.a@yandex.ru',
+            url='https://github.com/sidorenkov-v-a',
+            telegram='https://t.me/sidorenkov_vl'
+        ),
+        license=openapi.License(name='MIT License'),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns += [
+    path('swagger/', schema_view.with_ui(
+        'swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
+         name='schema-redoc'),
 ]
